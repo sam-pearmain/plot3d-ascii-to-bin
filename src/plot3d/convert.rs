2e00adda3a5bf80a3b3694531de8327846cae078
sam-pearmain <sam.pearmain@kcl.ac.uk>
2025-03-04 14:26:11 +0000

    added useful info when converting

--- src/plot3d/convert.rs
+++ src/plot3d/convert.rs
@@ -8,6 +8,17 @@ pub fn convert_plot3d_ascii_to_binary(input: &str, output: &str) -> io::Result<(
     // read the ascii plot3d file
     let blocks = read_plot3d_ascii(input)?;
 
+    // print info about the blocks
+    println!("found {} blocks", blocks.len());
+    for (i, block) in blocks.iter().enumerate() {
+        println!(" - block {}: dimensions {} x {} x {}",
+            i + 1,
+            block.imax,
+            block.jmax,
+            block.kmax,
+        );
+    }
+
     // write the binary plot3d file (using double precision)
     write_plot3d_binary(output, &blocks, true)?;
 
